c760b4387555bf3c1c00617299934ad6aabe12ee
Hans Dembinski <HDembinski@users.noreply.github.com>
2023-03-06 18:07:57 +0100

    Fix version and add `--version` to CLI (#2)
    * `crdb.__version__` used to display 0.0.0, regardless of the actual
    version.
    * Added `--version` option to command-line interface.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,15 @@
 from crdb.cli import main
+import crdb
 
 
 def test_main():
     main(["B/C"])
+
+
+def test_version(capsys):
+    try:
+        main(["--version"])
+    except SystemExit:
+        pass
+    c = capsys.readouterr()
+    assert c.out == f"crdb-{crdb.__version__}\n"

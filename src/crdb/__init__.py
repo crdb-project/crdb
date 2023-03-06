e01da721bb4f79a55990904ea34899f50018dd4d
Hans Dembinski <hans.dembinski@gmail.com>
2023-03-06 14:04:15 +0100

    list_references_support_multiple_quantities

--- src/crdb/__init__.py
+++ src/crdb/__init__.py
@@ -1,6 +1,20 @@
 __version__ = '0.0.0'
 
-from ._lib import query, clear_cache, experiment_masks, VALID_NAMES
+from ._lib import (
+    query,
+    clear_cache,
+    experiment_masks,
+    VALID_NAMES,
+    reference_urls,
+    bibliography,
+)
 
 
-__all__ = ["query", "clear_cache", "experiment_masks", "VALID_NAMES"]
+__all__ = [
+    "VALID_NAMES",
+    "query",
+    "clear_cache",
+    "experiment_masks",
+    "reference_urls",
+    "bibliography",
+]

bf19acf284f1ea39bf91c1c18d12b7bbfcbe8efd
Hans Dembinski <hans.dembinski@gmail.com>
2023-03-12 15:51:34 +0100

    missing files

---
+++ tests/test_mpl.py
@@ -0,0 +1,19 @@
+import pytest
+
+from crdb import all
+from crdb import mpl
+
+
+@pytest.fixture
+def table():
+    tab = all()
+    tab = tab[tab.quantity == "Li"]
+    return tab
+
+
+def test_draw_references(table):
+    mpl.draw_references(table)
+
+
+def test_draw_table(table):
+    mpl.draw_table(table)
